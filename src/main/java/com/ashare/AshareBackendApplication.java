a12d9e4427144e169f2501a8919f0571986aaf15
AshaJaneBrown <ashajanebrown@gmail.com>
2025-08-02 18:57:20 +0300

    Initial commit - Spring Boot backend for Ashare

---
+++ src/main/java/com/ashare/AshareBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ashare;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshareBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AshareBackendApplication.class, args);
+    }
+}
